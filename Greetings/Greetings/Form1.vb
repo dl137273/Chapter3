023690990f2288f1f1d9cca66fc1f0ff96c2a855
dl137273 <33095988+dl137273@users.noreply.github.com>
2017-11-03 11:30:34 -0500

    Created Greetings

---
+++ Greetings/Greetings/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub btnShowGreeting_Click(sender As Object, e As EventArgs) Handles btnShowGreeting.Click
+        Dim strName As String
+
+        strName = txtUsername.Text
+
+        lblGreeting.Text = "Hi " + strName
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
