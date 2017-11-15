71329076dd2db51d96a281550497249e90b55742
dl137273 <33095988+dl137273@users.noreply.github.com>
2017-11-15 10:29:31 -0600

    Revert "Created Celcius to Farenheit"
    This reverts commit 8b38c99bd0a87e837bd52eb16ee5535b04232a2b.

--- Test Score Average/Test Score Average/Form1.vb
+++ Test Score Average/Test Score Average/Form1.vb
@@ -1,23 +1,9 @@
 ﻿Public Class Form1
 
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-        Dim total, average As Double
+        Dim decAverage As Decimal
+        lblAverage = CDec((txtScore1.Text) + CDec(txtScore2.Text) + CDec(txtScore3.Text) + CDec(txtScore4.Text) + CDec(txtScore5.Text)) / 5
 
-        total = CDbl(txtScore1.Text) + CDbl(txtScore2.Text) + CDbl(txtScore3.Text) + CDbl(txtScore4.Text) + CDbl(txtScore5.Text)
-        average = total / 5
-
-        lblAverage.Text = average
-    End Sub
-
-    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
-        txtScore1.Text = ""
-        txtScore2.Text = ""
-        txtScore3.Text = ""
-        txtScore4.Text = ""
-        txtScore5.Text = ""
-    End Sub
-
-    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
-        Me.Close()
+        lblAverage.Text = decAverage
     End Sub
 End Class
